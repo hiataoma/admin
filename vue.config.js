0187b2742d42d8262ccbb3893462a09a74e9dca5
马海涛 <mahaitao@photonpay.com>
2021-07-29 17:22:49 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,32 @@ const path = require('path')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+
 const name = 'vue template' // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following method:
+// port = 9527 npm run dev OR npm run dev --port = 9527
+const port = process.env.port || process.env.npm_config_port || 8090 // dev port
+
 module.exports = {
   lintOnSave: false,
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  // lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    before: require('./mock/mock-server.js')
+  },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
