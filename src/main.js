1ddef32cb09e758328be09c94599086c3fffe109
马海涛 <mahaitao@photonpay.com>
2021-07-29 13:57:50 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+// import myPlugin from 'marvin_mahaitao_test_test'
 Vue.config.productionTip = false
+
+// Vue.use(myPlugin)
+
+Vue.use(Element, { size: 'mini' })
 
 new Vue({
   router,
